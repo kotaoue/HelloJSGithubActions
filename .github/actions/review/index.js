ec5a6440d29c633541b59c754c084acedb970d60
kota oue <kotaoue@gmail.com>
2022-01-06 17:18:37 +0900

    change data from login to id

--- .github/actions/review/index.js
+++ .github/actions/review/index.js
@@ -1,10 +1,9 @@
 const core = require('@actions/core');
 
 try {
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const userID = core.getInput('user-id');
+  console.log(`Hello ${userID}!`);
+  core.setOutput("target-user-id", userID);
 } catch (error) {
   core.setFailed(error.message);
 }
